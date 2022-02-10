091e39de7dcf289f72472474ed56969f36c78ee5
Michel Santos <michel.santos@webjump.com.br>
2022-02-09 23:12:10 -0300

    feature: Categories Crud Started

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
 import express from "express";
 
+import { categoriesRouter } from "./routes/categories.routes";
+
 const app = express();
 
-app.get("/", (request, response) => response.json({ message: "Olá Mundo" }));
+app.use(express.json());
+
+app.use("/categories", categoriesRouter);
 
 app.listen(3333, () => console.log("Server is Running"));
