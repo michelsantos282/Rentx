0e984130c9ed307009f57107b3aed5492afcf68d
Michel Santos <michel.santos@webjump.com.br>
2022-02-10 18:58:17 -0300

    feature: add repositories and services, create modules structure

---
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -0,0 +1,23 @@
+import { ISpecificationsRepository } from "../repositories/ISpecificationsRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+class CreateSpecificationService {
+  constructor(private specificationsRepository: ISpecificationsRepository) {}
+
+  execute({ name, description }: IRequest): void {
+    const categoryAlreadyExists =
+      this.specificationsRepository.findByName(name);
+
+    if (categoryAlreadyExists) {
+      throw new Error("Specification Already Exists");
+    }
+
+    this.specificationsRepository.create({ name, description });
+  }
+}
+
+export { CreateSpecificationService };
---
+++ src/routes/specifications.routes.ts
@@ -0,0 +1,27 @@
+import { Router } from "express";
+
+import { SpecificationsRepository } from "../modules/cars/repositories/SpecificationsRepository";
+import { CreateSpecificationService } from "../modules/cars/services/CreateSpecificationService";
+
+const specificationsRouter = Router();
+
+const specificationsRepository = new SpecificationsRepository();
+
+specificationsRouter.post("/", (request, response) => {
+  const { name, description } = request.body;
+  const createSpecificationService = new CreateSpecificationService(
+    specificationsRepository
+  );
+
+  createSpecificationService.execute({ name, description });
+
+  return response.status(201).send();
+});
+
+specificationsRouter.get("/", (request, response) => {
+  const categories = specificationsRepository.list();
+
+  return response.json(categories);
+});
+
+export { specificationsRouter };
