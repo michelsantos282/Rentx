19a1bba1d676ae36c8be0e42101914936da0a2a2
Michel Santos <michel.santos@webjump.com.br>
2022-02-15 21:39:09 -0300

    feature: create users flow

---
+++ src/routes/index.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+
+import { categoriesRouter } from "./categories.routes";
+import { specificationsRouter } from "./specifications.routes";
+import { usersRouter } from "./users.routes";
+
+const router = Router();
+
+router.use("/categories", categoriesRouter);
+router.use("/specifications", specificationsRouter);
+router.use("/users", usersRouter);
+
+export { router };
--- src/server.ts
+++ src/server.ts
@@ -2,20 +2,18 @@ import "reflect-metadata";
 import express from "express";
 import swaggerUi from "swagger-ui-express";
 
-import { categoriesRouter } from "./routes/categories.routes";
-import { specificationsRouter } from "./routes/specifications.routes";
 import swaggerFile from "./swagger.json";
 
 import "./database";
 import "./shared/container";
+import { router } from "./routes";
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+app.use(router);
 
-app.use("/categories", categoriesRouter);
-app.use("/specifications", specificationsRouter);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.listen(3333, () => console.log("Server is Running"));
