ad5c1c969b47a7f44f771a59a33973187b19152d
Michel Santos <michel.santos@webjump.com.br>
2022-02-16 23:44:25 -0300

    feature: create user authentication

---
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,39 @@
+import { hash } from "bcrypt";
+import { inject, injectable } from "tsyringe";
+
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+@injectable()
+class CreateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) {}
+
+  async execute({
+    name,
+    password,
+    email,
+    driver_license,
+  }: ICreateUserDTO): Promise<void> {
+    const emailAlreadyExists = await this.usersRepository.findByEmail(email);
+    const driverLicenseAlreadyExists =
+      await this.usersRepository.findByDriverLicense(driver_license);
+
+    if (emailAlreadyExists || driverLicenseAlreadyExists) {
+      throw new Error("User Already Exists");
+    }
+
+    const hashedPassword = await hash(password, 8);
+
+    await this.usersRepository.create({
+      name,
+      password: hashedPassword,
+      email,
+      driver_license,
+    });
+  }
+}
+
+export { CreateUserUseCase };
